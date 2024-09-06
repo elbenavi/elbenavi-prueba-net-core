23cf039f6b26355cf432abb876c10c3dead3dbcf
Eduardo Luis Benavides Muñoz <elbenavides@enerbit.co>
2024-09-06 09:41:28 -0500

    Fisrt commit

---
+++ BusinessLogic/Logic/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace BusinessLogic.Logic
+{
+    public class ProductRepository : IProductRepository
+    {
+        Task<IReadOnlyList<Product>> IProductRepository.GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Product> IProductRepository.GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿
+using BusinessLogic.Logic;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
